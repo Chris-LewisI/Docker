ed69944c616826b00ab2dee4ff90227024f12c65
Chris Ibraheem <cli14@scarletmail.rutgers.edu>
2020-09-30 23:05:45 -0400

    Adding NodeJS practice project

---
+++ NodeJS-App/src/index.js
@@ -0,0 +1,11 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Docker is easy 🐳'});
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`App is listening on http://localhost${port}`);
+});
